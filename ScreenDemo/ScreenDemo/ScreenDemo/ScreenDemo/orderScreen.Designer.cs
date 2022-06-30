2b28a2981936f3859cd530746f43159155811bc8
Joel Cantila <Joel.Cantila@mail.citytech.cuny.edu>
2022-06-29 21:07:38 -0400

    added some new buttons and click events.

--- ScreenDemo/ScreenDemo/ScreenDemo/ScreenDemo/orderScreen.Designer.cs
+++ ScreenDemo/ScreenDemo/ScreenDemo/ScreenDemo/orderScreen.Designer.cs
@@ -31,7 +31,6 @@
             this.pictureBox3 = new System.Windows.Forms.PictureBox();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.coffee_QuantityUpDown = new System.Windows.Forms.NumericUpDown();
             this.coffee_HotRadButton = new System.Windows.Forms.RadioButton();
             this.coffee_ColdRadButton = new System.Windows.Forms.RadioButton();
             this.coffee_QuantityLabel = new System.Windows.Forms.Label();
@@ -41,14 +40,18 @@
             this.radioButton1 = new System.Windows.Forms.RadioButton();
             this.radioButton2 = new System.Windows.Forms.RadioButton();
             this.label3 = new System.Windows.Forms.Label();
-            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.coffee_QuantityUpDown)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
             this.SuspendLayout();
             // 
             // pictureBox3
@@ -56,9 +59,10 @@
             this.pictureBox3.BackgroundImage = global::ScreenDemo.Properties.Resources.boba;
             this.pictureBox3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
             this.pictureBox3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.pictureBox3.Location = new System.Drawing.Point(35, 250);
+            this.pictureBox3.Location = new System.Drawing.Point(70, 481);
+            this.pictureBox3.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.pictureBox3.Name = "pictureBox3";
-            this.pictureBox3.Size = new System.Drawing.Size(150, 150);
+            this.pictureBox3.Size = new System.Drawing.Size(296, 285);
             this.pictureBox3.TabIndex = 2;
             this.pictureBox3.TabStop = false;
             this.pictureBox3.Click += new System.EventHandler(this.pictureBox3_Click);
@@ -68,9 +72,10 @@
             this.pictureBox2.BackgroundImage = global::ScreenDemo.Properties.Resources.coffee;
             this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
             this.pictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.pictureBox2.Location = new System.Drawing.Point(35, 35);
+            this.pictureBox2.Location = new System.Drawing.Point(70, 67);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(150, 150);
+            this.pictureBox2.Size = new System.Drawing.Size(296, 285);
             this.pictureBox2.TabIndex = 1;
             this.pictureBox2.TabStop = false;
             this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
@@ -79,31 +84,20 @@
             // 
             this.pictureBox1.BackgroundImage = global::ScreenDemo.Properties.Resources.Illustration;
             this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-            this.pictureBox1.Location = new System.Drawing.Point(232, 60);
+            this.pictureBox1.Location = new System.Drawing.Point(464, 115);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(511, 455);
+            this.pictureBox1.Size = new System.Drawing.Size(1022, 875);
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
-            // 
-            // coffee_QuantityUpDown
-            // 
-            this.coffee_QuantityUpDown.Location = new System.Drawing.Point(239, 130);
-            this.coffee_QuantityUpDown.Name = "coffee_QuantityUpDown";
-            this.coffee_QuantityUpDown.Size = new System.Drawing.Size(43, 20);
-            this.coffee_QuantityUpDown.TabIndex = 3;
-            this.coffee_QuantityUpDown.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
-            this.coffee_QuantityUpDown.Value = new decimal(new int[] {
-            1,
-            0,
-            0,
-            0});
             // 
             // coffee_HotRadButton
             // 
             this.coffee_HotRadButton.AutoSize = true;
-            this.coffee_HotRadButton.Location = new System.Drawing.Point(191, 105);
+            this.coffee_HotRadButton.Location = new System.Drawing.Point(382, 202);
+            this.coffee_HotRadButton.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.coffee_HotRadButton.Name = "coffee_HotRadButton";
-            this.coffee_HotRadButton.Size = new System.Drawing.Size(42, 17);
+            this.coffee_HotRadButton.Size = new System.Drawing.Size(76, 29);
             this.coffee_HotRadButton.TabIndex = 4;
             this.coffee_HotRadButton.TabStop = true;
             this.coffee_HotRadButton.Text = "Hot";
@@ -112,9 +106,10 @@
             // coffee_ColdRadButton
             // 
             this.coffee_ColdRadButton.AutoSize = true;
-            this.coffee_ColdRadButton.Location = new System.Drawing.Point(239, 105);
+            this.coffee_ColdRadButton.Location = new System.Drawing.Point(478, 202);
+            this.coffee_ColdRadButton.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.coffee_ColdRadButton.Name = "coffee_ColdRadButton";
-            this.coffee_ColdRadButton.Size = new System.Drawing.Size(46, 17);
+            this.coffee_ColdRadButton.Size = new System.Drawing.Size(87, 29);
             this.coffee_ColdRadButton.TabIndex = 5;
             this.coffee_ColdRadButton.TabStop = true;
             this.coffee_ColdRadButton.Text = "Cold";
@@ -123,45 +118,51 @@
             // coffee_QuantityLabel
             // 
             this.coffee_QuantityLabel.AutoSize = true;
-            this.coffee_QuantityLabel.Location = new System.Drawing.Point(188, 130);
+            this.coffee_QuantityLabel.Location = new System.Drawing.Point(376, 250);
+            this.coffee_QuantityLabel.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.coffee_QuantityLabel.Name = "coffee_QuantityLabel";
-            this.coffee_QuantityLabel.Size = new System.Drawing.Size(49, 13);
+            this.coffee_QuantityLabel.Size = new System.Drawing.Size(98, 25);
             this.coffee_QuantityLabel.TabIndex = 6;
             this.coffee_QuantityLabel.Text = "Quantity:";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(84, 19);
+            this.label1.Location = new System.Drawing.Point(168, 37);
+            this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.Size = new System.Drawing.Size(75, 25);
             this.label1.TabIndex = 7;
             this.label1.Text = "Coffee";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(84, 234);
+            this.label2.Location = new System.Drawing.Point(168, 450);
+            this.label2.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(26, 13);
+            this.label2.Size = new System.Drawing.Size(49, 25);
             this.label2.TabIndex = 8;
             this.label2.Text = "Tea";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(74, 428);
+            this.button1.Location = new System.Drawing.Point(148, 823);
+            this.button1.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(150, 44);
             this.button1.TabIndex = 9;
             this.button1.Text = "order now";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // radioButton1
             // 
             this.radioButton1.AutoSize = true;
-            this.radioButton1.Location = new System.Drawing.Point(195, 282);
+            this.radioButton1.Location = new System.Drawing.Point(390, 542);
+            this.radioButton1.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.radioButton1.Name = "radioButton1";
-            this.radioButton1.Size = new System.Drawing.Size(42, 17);
+            this.radioButton1.Size = new System.Drawing.Size(76, 29);
             this.radioButton1.TabIndex = 10;
             this.radioButton1.TabStop = true;
             this.radioButton1.Text = "Hot";
@@ -170,9 +171,10 @@
             // radioButton2
             // 
             this.radioButton2.AutoSize = true;
-            this.radioButton2.Location = new System.Drawing.Point(239, 282);
+            this.radioButton2.Location = new System.Drawing.Point(478, 542);
+            this.radioButton2.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.radioButton2.Name = "radioButton2";
-            this.radioButton2.Size = new System.Drawing.Size(46, 17);
+            this.radioButton2.Size = new System.Drawing.Size(87, 29);
             this.radioButton2.TabIndex = 11;
             this.radioButton2.TabStop = true;
             this.radioButton2.Text = "Cold";
@@ -181,54 +183,109 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(188, 322);
+            this.label3.Location = new System.Drawing.Point(376, 619);
+            this.label3.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(49, 13);
+            this.label3.Size = new System.Drawing.Size(98, 25);
             this.label3.TabIndex = 12;
             this.label3.Text = "Quantity:";
             // 
-            // numericUpDown1
-            // 
-            this.numericUpDown1.Location = new System.Drawing.Point(239, 320);
-            this.numericUpDown1.Name = "numericUpDown1";
-            this.numericUpDown1.Size = new System.Drawing.Size(43, 20);
-            this.numericUpDown1.TabIndex = 13;
-            this.numericUpDown1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
-            this.numericUpDown1.Value = new decimal(new int[] {
-            1,
-            0,
-            0,
-            0});
-            // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(74, 191);
+            this.button2.Location = new System.Drawing.Point(148, 367);
+            this.button2.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.Size = new System.Drawing.Size(150, 44);
             this.button2.TabIndex = 14;
             this.button2.Text = "order now";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(538, 428);
+            this.button3.Location = new System.Drawing.Point(1076, 823);
+            this.button3.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.Size = new System.Drawing.Size(150, 44);
             this.button3.TabIndex = 15;
             this.button3.Text = "Exit";
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(483, 250);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 31);
+            this.textBox2.TabIndex = 18;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(935, 24);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(178, 25);
+            this.label4.TabIndex = 19;
+            this.label4.Text = "Your Total Order:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(1138, 18);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(145, 31);
+            this.textBox1.TabIndex = 20;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(464, 823);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(199, 54);
+            this.button5.TabIndex = 22;
+            this.button5.Text = "Check Out";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(1024, 67);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(85, 25);
+            this.label5.TabIndex = 23;
+            this.label5.Text = "Price $:";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(1138, 63);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(145, 31);
+            this.textBox3.TabIndex = 24;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(478, 619);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 31);
+            this.textBox4.TabIndex = 25;
+            // 
             // orderScreen
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
-            this.ClientSize = new System.Drawing.Size(661, 480);
+            this.ClientSize = new System.Drawing.Size(1322, 923);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox2);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
-            this.Controls.Add(this.numericUpDown1);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.radioButton2);
             this.Controls.Add(this.radioButton1);
@@ -238,18 +295,16 @@
             this.Controls.Add(this.coffee_QuantityLabel);
             this.Controls.Add(this.coffee_ColdRadButton);
             this.Controls.Add(this.coffee_HotRadButton);
-            this.Controls.Add(this.coffee_QuantityUpDown);
             this.Controls.Add(this.pictureBox3);
             this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.pictureBox1);
             this.DoubleBuffered = true;
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.Name = "orderScreen";
             this.Text = "orderScreen";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.coffee_QuantityUpDown)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -260,7 +315,6 @@
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.PictureBox pictureBox2;
         private System.Windows.Forms.PictureBox pictureBox3;
-        private System.Windows.Forms.NumericUpDown coffee_QuantityUpDown;
         private System.Windows.Forms.RadioButton coffee_HotRadButton;
         private System.Windows.Forms.RadioButton coffee_ColdRadButton;
         private System.Windows.Forms.Label coffee_QuantityLabel;
@@ -270,8 +324,14 @@
         private System.Windows.Forms.RadioButton radioButton1;
         private System.Windows.Forms.RadioButton radioButton2;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.NumericUpDown numericUpDown1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
     }
 }
--- ScreenDemo/ScreenDemo/ScreenDemo/ScreenDemo/orderScreen.cs
+++ ScreenDemo/ScreenDemo/ScreenDemo/ScreenDemo/orderScreen.cs
@@ -16,33 +16,35 @@ namespace ScreenDemo
         {
             InitializeComponent();
         }
-
+        int coffeePrice = 4;
+        int teaPrice = 4;
+        int noOrder = 0;
         private void pictureBox2_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("You have selected Coffee");
+            //MessageBox.Show("You have selected Coffee");
 
-            if (coffee_HotRadButton.Checked)
-            {
-                MessageBox.Show("Hot coffee selected");
-            }
-            else
-            {
-                MessageBox.Show("Cold coffee selected");
-            }
+           // if (coffee_HotRadButton.Checked)
+           // {
+           //     MessageBox.Show("Hot coffee selected");
+           // }
+           // else
+           // {
+            //    MessageBox.Show("Cold coffee selected");
+            //}
         }
 
         private void pictureBox3_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("You have selected Tea");
+            //MessageBox.Show("You have selected Tea");
 
-            if (coffee_HotRadButton.Checked)
-            {
-                MessageBox.Show("Hot Tea selected");
-            }
-            else
-            {
-                MessageBox.Show("Cold Tea selected");
-            }
+           // if (coffee_HotRadButton.Checked)
+           // {
+           //     MessageBox.Show("Hot Tea selected");
+          //  }
+           // else
+           // {
+           //     MessageBox.Show("Cold Tea selected");
+           // }
         }
 
         private void button3_Click(object sender, EventArgs e)
@@ -59,6 +61,61 @@ namespace ScreenDemo
                 new orderScreen().Show();
                 this.Hide();
             }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (coffee_HotRadButton.Checked)
+            {
+                int coffee = int.Parse(textBox2.Text);
+                MessageBox.Show("You ordered: " + coffee + " hot coffee ");
+                
+            }
+            else if (coffee_ColdRadButton.Checked)
+            {
+                int coffee = int.Parse(textBox2.Text);
+                MessageBox.Show("You ordered: " + coffee + " cold coffee ");
+                
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked)
+            {
+                int tea = int.Parse(textBox4.Text);
+                MessageBox.Show("You ordered: " + tea + " hot tea ");
+
+            }
+            else if (radioButton2.Checked)
+            {
+                int tea = int.Parse(textBox4.Text);
+                MessageBox.Show("You ordered: " + tea + " cold tea ");
+
+            }
+           
+
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            int totalPrice3;
+            int totalOrder= int.Parse(textBox4.Text);
+            int totalTea = int.Parse(textBox2.Text);
+            totalOrder += totalTea;
+            textBox1.Text = totalOrder.ToString();
+
+            int totalPrice = int.Parse(textBox2.Text + coffeePrice);
+            int totalPrice2 = int.Parse(textBox4.Text + teaPrice);
+
+
+             totalPrice3 = (totalPrice + totalPrice2);
+            textBox3.Text = totalPrice3.ToString();
+            
+           
+
+
         }
     }
 }
